eab150d6dcb76ccb9ebfff2efd811714d17435f5
syrizelink <2532124404@qq.com>
2023-02-14 22:53:39 +0800

    解耦, 分离方法, 注解更新

--- src/main/java/syrize/Lyrics/FileOperation.java
+++ src/main/java/syrize/Lyrics/FileOperation.java
@@ -1,5 +1,7 @@
 package syrize.Lyrics;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -19,6 +21,7 @@ public class FileOperation {
             HomoValue,
             rhKey,
             lyricString,
+            singleValue,
             doubleChara,
             trueValue;
     static int
@@ -36,15 +39,15 @@ public class FileOperation {
      * @return 罗马字
      * @throws IOException ioexception
      */
-    public static List<String> getRomajiValue(List<String> x) throws IOException {
+    public static List<String> getRomajiValue(@NotNull List<String> x) throws IOException {
 
         i = 0;
         jsonRomaji = "Romaji";
         mainValue = "main";
         romajiValue = "Romaji";
         krKey = "KtoR";
-        List<String> kato = DataReading.mainJsonReading(jsonRomaji, krKey, mainValue);
-        List<String> romaji = DataReading.mainJsonReading(jsonRomaji, krKey, romajiValue);
+        List<String> kato = DataReading.mainJsonReading(jsonRomaji, krKey, mainValue);//将假名数据生成为列表
+        List<String> romaji = DataReading.mainJsonReading(jsonRomaji, krKey, romajiValue);//将罗马字数据生成为列表
 
         for (int a = 0; i < x.size() - 1; a++){
             StringBuilder sb = new StringBuilder(x.get(a));
@@ -60,6 +63,55 @@ public class FileOperation {
                 }
             }
             x.set(a, String.valueOf(sb));
+        }
+        return x;
+    }
+
+    /**
+     * 该方法通过两次遍历匹配单/双字符进行替换
+     * @param x 要进行操作的歌词行
+     * @return 替换完成后的歌词行
+     */
+    private static @NotNull String matchCharacter(String x) throws IOException {
+        jsonRomaji = "Romaji";
+        mainValue = "main";
+        romajiValue = "Romaji";
+        krKey = "KtoR";
+        List<String> kato = DataReading.mainJsonReading(jsonRomaji, krKey, mainValue);//将假名数据生成为列表
+        List<String> romaji = DataReading.mainJsonReading(jsonRomaji, krKey, romajiValue);//将罗马字数据生成为列表
+        StringBuilder sb = new StringBuilder(x);
+        i = 0;
+
+        for (; i < x.length() - 3; i++){
+            doubleChara = x.substring(i, i + 2);
+            trueValue = charaLocReplace(doubleChara, kato, romaji);
+            sb.replace(i, i + 2, trueValue);
+        }
+
+        i = 0;
+
+        for (; i < x.length() - 1; i++){
+            singleValue = x.substring(i, i + 1);
+            trueValue = charaLocReplace(singleValue, kato, romaji);
+            sb.replace(i, i + 1, trueValue);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 该方法用于在匹配替换给定双字符
+     * @param x 要进行操作的双字符
+     * @param matchList 比对匹配列表
+     * @return 替换完成的歌词行
+     */
+    private static String charaLocReplace(String x, @NotNull List<String> matchList, List<String> searchList){
+        i = 0;
+        for (; i < matchList.size(); i++){
+            String listValue = matchList.get(i);
+            if (listValue.equals(x)){
+                x = searchList.get(i);
+            }
         }
         return x;
     }
@@ -105,7 +157,7 @@ public class FileOperation {
      * @param x 需要判断的字符串
      * @return 布尔值
      */
-    public static boolean isJP(String x) {
+    public static boolean isJP(@NotNull String x) {
         Character.UnicodeScript script = Character.UnicodeScript.of(x.charAt(0));
         return script == Character.UnicodeScript.HIRAGANA || script == Character.UnicodeScript.KATAKANA || script == Character.UnicodeScript.HAN;
     }
